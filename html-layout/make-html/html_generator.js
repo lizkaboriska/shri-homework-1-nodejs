e8be43b8c1d227c8c317edfd26bb8e2b4a44f4de
Elizaveta Borisova <lizkaboriska@gmail.com>
2019-09-29 16:04:21 +0300

    Add redux bundle to html

--- html-layout/make-html/html_generator.js
+++ html-layout/make-html/html_generator.js
@@ -8,6 +8,8 @@ exports.buildHtml = function(body) {
             '<link rel="stylesheet" href="style.css">\n' +
             '</head>' +
             '<body>' +
+            '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>' +
+            '<script src="../redux/dist/index.bundle.js"></script>' +
              body +
             '<script type="text/javascript" src="script.js"></script>' +
             '</body>' +
