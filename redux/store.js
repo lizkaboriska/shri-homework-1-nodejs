923b6e250ce8ee96c15fa628cb8db6c3f246adbd
Elizaveta Borisova <lizkaboriska@gmail.com>
2019-09-29 16:02:53 +0300

    Add redux realization

--- redux/store.js
+++ redux/store.js
@@ -23,45 +23,7 @@ function createStore(reducer, initial_state ) {
     };
 }
 
-const InitialState = {
-    a: 0,
-    b: 1,
-    c: 2
+module.exports = {
+    "createStore": createStore
 }
-
-//console.log(InitialState.a + InitialState.b);
-
-function reducer(state = InitialState, action) {
-    switch (action.type) {
-        case "PLUS_ONE":
-            state.a = state.a + action.payload;
-            state.b = state.b + action.payload;
-            state.c = state.c + action.payload;
-        case "MINUS_TWO":
-            state.a = state.a - action.payload;
-            state.b = state.b - action.payload;
-            state.c = state.c - action.payload;
-    }
-    return state;
-}
-
-let store = createStore(reducer, InitialState);
-
-console.log(store.getState());
-
-let plusOne = {
-    type: "PLUS_ONE",
-    payload: 1
-}
-let plusTwo  = {
-    type: "MINUS_TWO",
-    payload: 2
-}
-
-
-store.dispatch(plusOne);
-console.log(store.getState());
-
-store.dispatch(plusTwo);
-console.log(store.getState());
 
